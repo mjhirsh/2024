7557b0b58d7531cc4a87c72c487aff8452945172
mjhirsh <marjuhirsh@gmail.com>
2024-12-05 19:11:44 +0200

    Add sex idintifier as first character

--- exercise/ts/day05/tests/eid.spec.ts
+++ exercise/ts/day05/tests/eid.spec.ts
@@ -1,10 +1,31 @@
-const makeEid = () => {
-  return '11111111'
+const makeEid = (sex) => {
+  const sexMap = new Map([
+    ['Sloubi', '1'],
+    ['Gagna', '2'],
+    ['Catact', '3'],
+  ])
+
+  const sexCode = sexMap.get(sex)
+
+  return sexCode + '1111111'
 }
 
 describe('EID', () => {
   test('eid should be 8 digits long', () => {
-    const eid = makeEid()
+    const eid = makeEid('Sloubi')
     expect(eid.length).toBe(8)
+  })
+
+  test('the first character of eid should be a valid sex', () => {
+    const sex = new Map([
+      ['Sloubi', '1'],
+      ['Gagna', '2'],
+      ['Catact', '3'],
+    ])
+
+    sex.forEach((value, key) => {
+      const eid = makeEid(key)
+      expect(eid[0]).toBe(value)
+    })
   })
 })
