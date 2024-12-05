cbe1a90268205ffd270781bd6345987fe50dbe7f
mjhirsh <marjuhirsh@gmail.com>
2024-12-05 20:52:14 +0200

    Add key to eid

--- exercise/ts/day05/tests/eid.spec.ts
+++ exercise/ts/day05/tests/eid.spec.ts
@@ -1,19 +1,30 @@
-const makeEid = (sex, birthYear) => {
-  const sexMap = new Map([
-    ['Sloubi', '1'],
-    ['Gagna', '2'],
-    ['Catact', '3'],
-  ])
+const eidFactory = () => {
+  let birthOrder = 1
 
-  const sexCode = sexMap.get(sex)
-  const birhYearCode = birthYear.slice(-2)
+  return (sex, birthYear) => {
+    const sexMap = new Map([
+      ['Sloubi', '1'],
+      ['Gagna', '2'],
+      ['Catact', '3'],
+    ])
 
-  return sexCode + birhYearCode + '11111'
+    let eid = ''
+
+    eid += sexMap.get(sex)
+    eid += birthYear.slice(-2)
+    eid += birthOrder.toString().padStart(3, '0')
+    eid += 97 - (parseInt(eid) % 97)
+    birthOrder++
+    return eid
+  }
 }
 
 describe('EID', () => {
+  const name = 'Sloubi'
+  const birthYear = '1990'
+  const makeEid = eidFactory()
   test('eid should be 8 digits long', () => {
-    const eid = makeEid('Sloubi', '1990')
+    const eid = makeEid(name, birthYear)
     expect(eid.length).toBe(8)
   })
 
@@ -25,14 +36,30 @@ describe('EID', () => {
     ])
 
     sex.forEach((value, key) => {
-      const eid = makeEid(key, '1990')
+      const eid = makeEid(key, birthYear)
       expect(eid[0]).toBe(value)
     })
   })
 
   test('the second two characters should be birth year', () => {
-    const birthYear = '1990'
-    const eid = makeEid('Sloubi', birthYear)
-    expect(eid.slice(1, 3)).toBe('90')
+    const birthYear = '1687'
+    const eid = makeEid(name, birthYear)
+    expect(eid.slice(1, 3)).toBe('87')
+  })
+
+  test('4,5,6 characters should represent birth order', () => {
+    const makeEid = eidFactory()
+    for (let i = 1; i < 1000; i++) {
+      const birthOrder = i.toString().padStart(3, '0')
+      const eid = makeEid('Sloubi', '1990')
+      expect(eid.slice(3, 6)).toBe(birthOrder)
+    }
+  })
+
+  test('the last two characters as a number added to modulo 97 of the first six numbers should be 97', () => {
+    const eid = makeEid(name, birthYear)
+    const first6AsNumber = eid.slice(0, 6)
+    const key = 97 - (parseInt(first6AsNumber) % 97)
+    expect(eid.slice(-2)).toBe(String(key))
   })
 })
