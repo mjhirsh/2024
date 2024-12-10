482e440b681663f17116ba1c69c2e4b544ed3216
mjhirsh <marjuhirsh@gmail.com>
2024-12-10 18:16:25 +0200

    Make val a number array

--- exercise/ts/day10/src/delivery/building.ts
+++ exercise/ts/day10/src/delivery/building.ts
@@ -1,6 +1,6 @@
 export class Building {
     static whichFloor(instructions: string): number {
-        let val: Array<[number]> = [];
+        let val: Array<number> = [];
 
         for (let i = 0; i < instructions.length; i++) {
             const c = instructions[i];
@@ -8,15 +8,15 @@ export class Building {
                 continue;
             } else if (instructions.includes('🧝')) {
                 const j = c === ')' ? 3 : -2;
-                val.push([j]);
+                val.push(j);
             } else if (!instructions.includes('🧝')) {
-                val.push([c === '(' ? 1 : -1]);
+                val.push(c === '(' ? 1 : -1);
             }
         }
 
         let result = 0;
         for (const kp of val) {
-            result += kp[0];
+            result += kp;
         }
 
         return result;
